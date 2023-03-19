9090e882d0c982ba6f5aab0be8d9a42bdc6e6d97
Filipe Santos <filipesantos1999@hotmail.com>
2023-03-19 16:18:06 +0000

    Add distribute task class

---
+++ src/tasks/distributetask.nim
@@ -0,0 +1,44 @@
+# nim
+import interfaces/[
+    taskfile,
+    taskmathcalculation,
+    taskos,
+    taskparser
+]
+
+
+type
+    DistributeTask* = ref object
+
+proc newDistributeTask*(): DistributeTask =
+    return DistributeTask()
+
+proc distributeTask*(
+    self: DistributeTask,
+    service: TaskFile,
+    arg: string
+): string =
+    if arg.len() == 0:
+        return "NO ARG"
+
+    return service.get(arg)
+
+proc distributeTask*(
+    self: DistributeTask,
+    service: TaskMathCalculation,
+    arg: uint32
+): uint64 =
+    return service.compute(arg)
+
+proc distributeTask*(
+    self: DistributeTask,
+    service: TaskOS
+): string =
+    return service.process()
+
+proc distributeTask*(
+    self: DistributeTask,
+    service: TaskParser,
+    arg: ref object
+): string =
+    return service.parse(arg)
